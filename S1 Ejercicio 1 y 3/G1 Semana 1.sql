09c629fddb5b50bcc5d32dbe82617ccda37189d8
DANIEL-MANCIA <dmancia299@gmail.com>
2024-10-13 19:16:12 -0600

    Procesos alamcenado - CRUD Terminado

--- S1 Ejercicio 1 y 3/G1 Semana 1.sql
+++ S1 Ejercicio 1 y 3/G1 Semana 1.sql
@@ -53,3 +53,116 @@ go
 -- Prueba de la funcion: categoria 2
 select *from ListarProductosPorCategoria(2);
 go
+
+-- ********* PROCESOS ALMACENADOS - CRUD *********
+
+-- ******************************
+-- ****** INSERT CLIENTE ********
+-- ******************************
+
+create or alter procedure InsertarCliente
+    @NombresCliente varchar(100),
+    @ApellidosCliente varchar(100),
+    @DuiCliente char(10),
+    @TelefonoCliente varchar(15),
+    @CorreoCliente varchar(100),
+    @IdDireccion int
+as
+begin
+	insert into
+	Cliente.Clientes 
+	(NombresCliente, ApellidosCliente, DuiCliente, TelefonoCliente, CorreoCliente, IdDireccion) values 
+	(@NombresCliente, @ApellidosCliente, @DuiCliente, @TelefonoCliente, @CorreoCliente, @IdDireccion);
+end;
+go
+
+exec InsertarCliente 
+    'Rodrigo Alejandro',     -- NombresCliente
+    'Valencia Ruiz',         -- ApellidosCliente
+    '01234321-9',            -- DuiCliente
+    '+503 77378286',         -- TelefonoCliente
+    'alevalencia@gmail.com', -- Correo
+    4;                       -- Id_Direccion
+go
+
+select *from Cliente.Clientes;
+go
+
+-- ***************************
+-- ****** UPDATE CLIENTE *****
+-- ***************************
+
+create or alter procedure ActualizarCliente(
+    @IdCliente int,
+	@NombresCliente varchar(100),
+    @ApellidosCliente varchar(100),
+    @DuiCliente char(10),
+    @TelefonoCliente varchar(15),
+    @CorreoCliente varchar(100),
+    @IdDireccion int
+)as
+begin
+	update Cliente.Clientes 
+	set NombresCliente = @NombresCliente, 
+		ApellidosCliente = @ApellidosCliente, 
+		@DuiCliente = DuiCliente, 
+		TelefonoCliente = @TelefonoCliente, 
+		CorreoCliente = @CorreoCliente, 
+		IdDireccion = @IdDireccion
+	where IdCliente = @IdCliente;
+end;
+go
+
+-- Prueba: Actualizar el primer nombre, segundo apellido, la direccion al cliente que agg recientemente.
+exec ActualizarCliente 
+	9,						 -- IdCliente
+    'Lucas Alejandro',       -- NombresCliente
+    'Valencia Calderon',     -- ApellidosCliente
+    '01234321-9',            -- DuiCliente
+    '+503 77378286',         -- TelefonoCliente
+    'alevalencia@gmail.com', -- Correo
+    2;                       -- Id_Direccion
+go
+
+select *from Cliente.Clientes;
+go
+
+-- ********************************
+-- ****** ELIMINAR CLIENTE ********
+-- ********************************
+
+create or alter procedure EliminarCliente(
+	@id int
+) as
+begin
+	delete from Cliente.Clientes
+	where IdCliente = @id;
+end;
+go
+
+-- Prueba: Eliminar el cliente que hemos actualizado en el paso anterior
+exec EliminarCliente 
+	9;		-- IdCliente a eliminar
+go
+
+select *from Cliente.Clientes;
+go
+
+-- ********************************
+-- ****** SELECT CLIENTE ********
+-- ********************************
+create or alter procedure ObtenerClientesConFiltro
+    @Filtro nvarchar(50)
+as
+begin
+    select IdCliente, NombresCliente, ApellidosCliente, TelefonoCliente, CorreoCliente
+    from Cliente.Clientes
+	-- Recupera los clientes donde nombre o apellido coincidan con el texto que pasaremos en la variables @filtro
+    where NombresCliente like '%' + @Filtro + '%' or ApellidosCliente like '%' + @Filtro + '%'
+    order by NombresCliente;
+end;
+go
+
+-- Prueba: Apellido: Rodriguez = Ro.
+exec ObtenerClientesConFiltro 'Ro';
+go
